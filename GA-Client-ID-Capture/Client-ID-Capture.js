eec0bcd96678f6dc9e832dd696abb22bd783ff17
Yanir Calisar <ycalisar@gmail.com>
2018-05-02 07:52:30 -0700

    fixed a bug in UTM-tracking.js updating copyrights text

--- GA-Client-ID-Capture/Client-ID-Capture.js
+++ GA-Client-ID-Capture/Client-ID-Capture.js
@@ -1,6 +1,8 @@
-jQuery(document).ready(function(){
+(function(){
     var FIELD_NAME = "gAClientID";
+    var CID = ga.getAll()[0].get('clientId');
+    console.log(CID);
     jQuery('input[name=' + FIELD_NAME + ']').each(function () {
-        jQuery(this).val(ga.getAll()[0].get('clientId'));
+        jQuery(this).val(CID);
     });
-})
+})();
