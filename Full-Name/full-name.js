528c8074fc558796c14454a33e25df1514d102f4
Yanir Calisar <ycalisar@gmail.com>
2019-03-06 07:58:53 +0200

    minor

--- Full-Name/full-name.js
+++ Full-Name/full-name.js
@@ -1,11 +1,3 @@
-/*
- * Copyright (c), Yanir Calisar (ycalisar at overstack.io)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this code and associated documentation files, to deal in the code without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the code.
- * The user assumes responsibility for determining appropriate use of the code, for consequences of its use, and for checking functionality against other reliable sources.
- * We suggest citation in publications as with any code developement work. No warrantee is given.
- * Please help improve the code by sending suggestions or new code back.
- */
 (function(){
 	if(window.splitFullNameActive || !window.MktoForms2) return;
 	else window.splitFullNameActive = true;
@@ -105,3 +97,9 @@
 		splitFullName("FirstName", "LastName", "leadFullName");
 	});
 })();
+
+
+/*
+ * Copyright (c), Yanir Calisar (ycalisar at overstack.io)
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this code and associated documentation files, to deal in the code without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the code. The user assumes responsibility for determining appropriate use of the code, for consequences of its use, and for checking functionality against other reliable sources. We suggest citation in publications as with any code developement work. No warrantee is given. Please help improve the code by sending suggestions or new code back.
+ */
