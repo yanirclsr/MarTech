49140a3d5fa836de6c0fcc2b971488051dc3a0c3
Yanir Calisar <ycalisar@gmail.com>
2019-12-10 16:13:50 +0200

    minor fix

--- Google-Scripts/Quora-Stats/Quora_Stats_for_Google_Sheets.js
+++ Google-Scripts/Quora-Stats/Quora_Stats_for_Google_Sheets.js
@@ -3,44 +3,78 @@
 * @customfunction
 */
 
+var headers = {
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
+    "sec-fetch-user": "?1",
+    "sec-fetch-site":"cross-site",
+    "upgrade-insecure-requests": 1,
+    "pragma": "no-cache"
+}
+
+var options =  {
+    "method" : "get",
+    "muteHttpExceptions": true,
+    "headers": headers
+};
+
+//the function that exposed in the sheet
 function quora(url) {
 
 
     var vals = [];
-    url = !url ? "https://www.quora.com/What-is-Quora-112":url;
 
+    if(url != undefined && url.indexOf("quora.com") > 0) {
 
-    var res = UrlFetchApp.fetch(url);
-    var content = res.getContentText();
+        var res = UrlFetchApp.fetch(url + "?" + new Date().getDate(), options);
+        var content = res.getContentText();
 
-    var xmldoc = Xml.parse(content, true);
-    var title = xmldoc.html.head.getElements("title")[0].getText().replace(" - Quora","").replace(/"/g, "'");
+        var xmldoc = Xml.parse(content, true);
+        var title = getTitle(xmldoc, content);
 
-    if(title && title.length > 0){
+        if (title && title.length > 0) {
 
-        vals.push(url);
-        vals.push(title);
-        vals.push(getTopics(content));
-        vals.push(getAnswers(content));
+            vals.push(url);
+            vals.push(title);
+            vals.push(getTopics(content));
+            vals.push(getAnswers(content));
 
-        var logs = getLogs(url);
-//    vals.push(logs);
-        vals.push(logs.views);
-        vals.push(logs.followers);
+            var logs = getLogs(url);
+            vals.push(logs.views);
+            vals.push(logs.followers);
 
-        return [vals];
+            return [vals];
 
-    }else{
-        return [["Failed to get data! Check your link."]];
+        } else {
+            return [["Failed to get data! Check your link."]];
+        }
+    }else
+        return [["Please provide a valid link to some Quora question"]];
+
+}
+
+function getTitle(xmldoc, content){
+    try{
+        var t = "unknown!";
+        var elm = xmldoc && xmldoc.html && xmldoc.html.head ? xmldoc.html.head.getElements("title") : "";
+        if(elm.length > 0){
+            t = elm[0].getText();
+        }else{
+            var c = content.substring(content.indexOf("h1>"),content.indexOf("h1>") + 50);
+            t = c;
+        }
+
+        return t.replace(" - Quora","").replace(/"/g, "'");
+//        return [0].getText().replace(" - Quora","").replace(/"/g, "'");
+    }catch(e){
+        return "unknown";
     }
-
 }
 
 function getLogs(url){
 
-    url = removeLastSlashIfExists(url) + "/log";
+    url = removeLastSlashIfExists(url) + "/log" + "?" + new Date().getDate();
 
-    var html = UrlFetchApp.fetch(url).getContentText();
+    var html = UrlFetchApp.fetch(url, options).getContentText();
     var data = {
         views: getViews(html),
         followers: getFollowers(html)
@@ -52,9 +86,7 @@ function getLogs(url){
 
 function getTopics(html){
 
-    //QuestionTopicListItems
     try{
-
         var txt = "";
         var elms = html.substring(html.indexOf("QuestionTopicListItems") + 20,html.indexOf("QuestionTopicListItems") + 3000).split("TopicNameSpan");
         for(var i =1; i < elms.length; i ++){
@@ -67,8 +99,6 @@ function getTopics(html){
 
         return txt;
 
-
-
     }catch(e){
         return 0;
     }
@@ -76,12 +106,15 @@ function getTopics(html){
 }
 
 function getAnswers(html){
+
     try{
         var str = html.substring(html.indexOf("answer_count") + 10,html.indexOf("answer_count") + 30 ).replace(/\D/g,'');
         if(str == 100){
             return "100+";
-        }else{
+        }else if(str.length > 0){
             return str;
+        }else{
+            return "0";
         }
     }catch(e){
         return 0;
@@ -90,7 +123,9 @@ function getAnswers(html){
 
 function getViews(html){
     try{
-        return html.substring(html.indexOf(" Views") - 10,html.indexOf(" Views") ).replace(/\D/g,'');
+        var e = html.substring(html.indexOf(" Views") - 10,html.indexOf(" Views") ).replace(/\D/g,'');
+        e = e.length > 0 ? e : "0";
+        return e;
     }catch(e){
         return 0;
     }
@@ -98,13 +133,13 @@ function getViews(html){
 
 function getFollowers(html){
     try{
-        return html.substring(html.indexOf("Public Followers") - 10, html.indexOf("Public Followers") ).replace(/\D/g,'');
+        var e = html.substring(html.indexOf("Public Followers") - 10, html.indexOf("Public Followers") ).replace(/\D/g,'');
+        e = e.length > 0 ? e : "0";
+        return e;
     }catch(e){
         return 0;
     }
 }
-
-
 
 function removeLastSlashIfExists(url){
     var slash = url.substring(url.length -1, url.length);
